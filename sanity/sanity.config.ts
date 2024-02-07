ff09763b17ee7c38aad8fe110a1fe79aea3b63ca
kodaneflash <jamesonmarcher@gmail.com>
2024-02-06 19:21:25 -0500

    modified:   app/blog/[slug]/page.tsx modified:   app/components/Navbar.tsx modified:   app/lib/sanity.ts modified:   sanity/sanity.cli.ts modified:   sanity/sanity.config.ts

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -5,9 +5,9 @@ import {schemaTypes} from './schemas'
 
 export default defineConfig({
   name: 'default',
-  title: 'nextjs-14-blog',
+  title: 'DevBlock',
 
-  projectId: 'sw0fgp2s',
+  projectId: 'eicpgrzh',
   dataset: 'production',
 
   plugins: [deskTool(), visionTool()],
